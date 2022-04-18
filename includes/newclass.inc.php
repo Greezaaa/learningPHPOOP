94da06a0e559b690e4abae93d961498f0e8e09a4
Jeka <greezaaa@gmail.com>
2022-04-18 02:07:32 +0200

    creating class with public properties

---
+++ includes/newclass.inc.php
@@ -0,0 +1,12 @@
+<?php
+//creando nueva clase con nombre "NewClass"
+class NewClass
+{
+
+    //propiedades y metodos van aqui
+    public $info = "Esto es una propiedad publica";
+}
+ //convertimos todo contenido de la clase en variable $objets
+ $object = new NewClass;
+
+ var_dump($object);
