ab0d1dd9f3ccecd7820e764d90aaabe69b381061
Jeka <greezaaa@gmail.com>
2022-04-18 12:44:32 +0200

    clase 05 Propiedades y Metodos

--- 06OOPPHP-Constuctors/index.php
+++ 06OOPPHP-Constuctors/index.php
@@ -10,22 +10,15 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title></title>
 </head>
 
 <body>
 
     <?php
     
-     $Jeka = new Person;
-        $Jeka->setName("Jeka", "azul");
-        echo $Jeka->name . " tiene ojos de color ";
-        echo $Jeka->colorOjos . "<br>";
-    
-        $Fernando = new Person;
-        $Fernando->setName("Fernando", "azul");
-        echo $Fernando->name . " tiene ojos de color ";
-        echo $Fernando->colorOjos;
+    $person = new Person();
+
      ?>
 
 
