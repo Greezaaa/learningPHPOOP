8728e5d48660fd688331582bb07dac48a54c13a3
Jeka <greezaaa@gmail.com>
2022-04-18 02:32:13 +0200

    Clases con prop o metodos privados o protegidos

--- includes/newclass.inc.php
+++ includes/newclass.inc.php
@@ -9,4 +9,42 @@ class NewClass
  //convertimos todo contenido de la clase en variable $objets
  $object = new NewClass;
 
- var_dump($object);
+
+
+
+
+ class PersonalName
+ {
+     //las propiedades privades se puede acceder solo de la misma clase en la que estan
+     private $first = "Daniel";
+     protected $last = "Fernandez";
+     private $age = "29";
+
+     public function nombre()
+     {
+         $a =$this->first;
+         return $a;
+     }
+ }
+
+
+ 
+ $person = new PersonalName();
+//deberia mostrar el nombre "Daniel;
+ echo $person -> nombre();
+ echo "<br>";
+ 
+ //herreddar propiedades de una clase
+class Last extends PersonalName
+{
+    public function apellido()
+    {
+        $b =$this->last;
+        return $b;
+    }
+}
+
+$person = new Last();
+//deberia mostrar el apellido "Fernandez";
+echo $person -> apellido();
+echo "<br>";
