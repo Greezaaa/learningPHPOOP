2bdc576dee3d0c47a42c332fc54940c4aedaa25f
Jeka <greezaaa@gmail.com>
2022-04-19 14:09:12 +0200

    clase 09 - Autoload Classes

--- 09OOPPHP - Load Classes Automaticaly/index.php
+++ 09OOPPHP - Load Classes Automaticaly/index.php
@@ -9,7 +9,7 @@ include "inc/autoloader.inc.php";
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Title </title>
+    <title>09 OOPPHP - Autoload Classes</title>
 </head>
 
 <body>
